bd460a394978ae1dc8467d047633833f557c7ec0
Isaac-Muia <isaac_muia@hotmail.com>
2022-08-25 10:35:31 +0800

    renamed file io file

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:geolocator/geolocator.dart';
+import 'file_io.dart';
 
 void main() {
   runApp(const MyApp());
